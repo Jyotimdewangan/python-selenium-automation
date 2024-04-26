ec46f25b84e32a95c6d4666db9f320c8706dd1e1
jyotimdewangan <jyotimdewangan@github.com>
2024-04-25 21:17:20 -0700

    Homework Lesson 3 Part 1

--- features/steps/target_cart_steps_lesson3.py
+++ features/steps/target_cart_steps_lesson3.py
@@ -24,5 +24,6 @@ def verify_cart_empty_message(context):
     actual_text = context.driver.find_element(*CART_EMPTY_MSG).text
     expected_text = 'Your cart is empty'
     assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
+    #assert actual_text.lower() in expected_text, f'Expected {expected_text}, but got {actual_text}'
     #context.driver.find_element(By.XPATH, "//h1[text()='Your cart is empty']")
 
